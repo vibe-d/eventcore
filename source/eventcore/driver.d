52c00b422f259515f8e63e8b94fa619cf15aa6ae
Carsten Schlote <schlote@vahanus.net>
2021-07-26 10:49:50 +0200

    Changes from review comments

--- source/eventcore/driver.d
+++ source/eventcore/driver.d
@@ -323,7 +323,8 @@ interface EventDriverSockets {
 				specified and the target address parameter of `send` is
 				left to `null`, it will be used instead.
 			options = Optional options for datagram creation. If unset,
-				'DatagramCreateOptions.init' is used.
+				`DatagramCreateOptions.init` is used.
+
 		Returns:
 			Returns a datagram socket handle if the socket was created
 			successfully. Otherwise returns `DatagramSocketFD.invalid`.
