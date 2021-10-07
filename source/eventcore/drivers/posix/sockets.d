e1fb002258c04018c0bab55cd8f5965ab7e4b1d2
Hiroki Noda <kubo39@gmail.com>
2021-10-07 10:02:03 +0900

    Simplified getBacklogSize

--- source/eventcore/drivers/posix/sockets.d
+++ source/eventcore/drivers/posix/sockets.d
@@ -1202,18 +1202,20 @@ private int getSocketError()
 
 private int getBacklogSize()
 @trusted @nogc nothrow {
-	int backlog = 128;
 	version (linux)
 	{
-		import core.stdc.stdio : fclose, fopen, fscanf;
-		auto somaxconn = fopen("/proc/sys/net/core/somaxconn", "re");
-		if (somaxconn)
-		{
-			int tmp;
-			if (fscanf(somaxconn, "%d", &tmp) == 1)
-				backlog = tmp;
-			fclose(somaxconn);
-		}
+		// https://github.com/torvalds/linux/commit/19f92a030ca6d772ab44b22ee6a01378a8cb32d4
+		// > Many applications indeed setup a TCP listener with listen(fd, -1);
+		// > meaning they let the system select the backlog.
+		return -1;
 	}
-	return backlog;
+	else version (Posix)
+	{
+		return SOMAXCONN;
+	}
+	else version (Windows)
+	{
+		return 128;
+	}
+	else static assert(false, "Unknown platform.");
 }
